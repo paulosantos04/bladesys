d82a62f8b3e790322cac39ef2045922ac3ca9d15
Paulo Santos <paulosantosdev@outlook.com>
2025-05-28 02:03:00 +0000

    Continuando configuração do front-end do projeto

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template #Importando classe Flask e Render_Templ
 app = Flask(__name__) #Criando aplicação Flask
 
 @app.route("/") #Define rota principal
-def login(): #Função ára página de login
-    return render_template("login.html") #Retorno da função login, retorna HTML da página de login
+def index(): #Função para página principal
+    return render_template("index.html") #Retorno da função index, retorna HTML da página principal
 
 @app.route("/planos") #Define rota para página de planos
 def planos(): #Função para página de planos
@@ -14,9 +14,9 @@ def planos(): #Função para página de planos
 def agenda(): #Função para página de agendamento
     return render_template("agendamento.html") #Retorno da função agenda, retorna HTML da página de agendamento
 
-#@app.route("/login") #Define rota para página de login
-#def login(): #Função para página de login
-    #return render_template("login.html") #Retorno da função login, retorna HTML da página de login
+@app.route("/login") #Define rota para página de login
+def login(): #Função para página de login
+    return render_template("login.html") #Retorno da função login, retorna HTML da página de login
 
 if __name__ == "__main__": #Compara se a aplicação criada é a principal
     app.run(debug=True) #Inicia servidor em modo de depuração (a cada alteração o servidor atualiza)
